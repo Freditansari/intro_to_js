6548e54385a6a188ad6afe36a52dd9945b9b552f
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-06-23 20:53:41 +0800

    added intro to classes

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-alert("hello world")
+
+
+
+
+const car= {
+    manufacturer: "toyota",
+    make: "Corolla", 
+    productionYear: 1984,
+    isActive: true,
+    isToyota : function (){
+        if(this.manufacturer==="toyota"){
+            return true
+        }
+        return false
+    }
+}
+
+class Car {
+constructor(manufacturer, make, productionYear, isActive){
+    this.manufacturer = manufacturer, 
+    this.make= make, 
+    this.productionYear= productionYear, 
+    this.isActive = isActive
+}
+
+    isToyota(){
+        if(this.manufacturer==="toyota"){
+            return true
+        }
+        return false
+    }
+
+    returnName(){
+        return this.manufacturer + " "+ this.make
+    }
+}
+
+
+console.log(car.isToyota());
+
+newCar = new Car("Nissan", "Trueno", "1986", true)
+console.log(newCar.isToyota())
+console.log(newCar)
+console.log(newCar.make)
