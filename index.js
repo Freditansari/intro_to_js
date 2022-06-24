540a67674114231a0ca6f9971dedb97e18515373
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-06-24 16:29:29 +0800

    added

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-// arrow functions
-// classic functons 
-// declrataion 
-// arguments
-// return value
-
-
-class Car{
-    constructor(manufacturer, make, productionYear, isActive){
-        this.manufacturer= manufacturer, 
-        this.make = make,
-        this.productionYear = productionYear,
-        this.isActive = isActive
-    }
-
-    isToyota(){
-        if(this.manufacturer === "toyota"){
-            return true
-        }
-        return false
-    }
-
-    returnName(){
-        return this.manufacturer + " " + this.make
-    }
-}
-
-let car = new Car("nissan", "Sentra", 1984, true)
-
-console.log(car)
-console.log(car.isToyota())
-
-
-console.log(car.returnName())
+// if then else 
+// for loop
+// while loop
+// ternary operator
