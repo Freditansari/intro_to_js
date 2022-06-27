b278c808732ac2b2c0f84463e9375684bae668d1
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-06-27 18:19:37 +0800

    messaege

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let customers=["Joe", {customerName: "joe", age: 43}, 25.4, 1]
+let customers=[ 
+    {customerName: "Joe", age: 43},
+    {customerName: "Alex", age: 22} 
+]
 
-console.log(customers)
+// let newCustomer = {customerName: "Brianna", age: 24}
 
-console.log(customers[2])
+let newCustomer = [{customerName: "Brianna", age: 24},
+ {customerName: "Diane", age: 34}]
 
-for(customer in customers){
-    // console.log(customer)
-    if(customers[customer]==="Joe"){
-        console.log(customers[customer])
-    }
-    
-}
+// let newCustomersList = customers.concat(newCustomer)
+
+//ES-5 2009 version javascript
+let newCustomersList = customers.concat(newCustomer);
+
+console.log(newCustomersList)
+
+// Es- 6 2015 v js: 
+
+let newCustomersList2 = [...customers, ...newCustomer]
+
+console.log(newCustomersList2)
