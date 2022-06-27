19b0933aab0d4e92261d33a6156702f57bfdefc1
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-06-27 20:26:47 +0800

    message

--- index.js
+++ index.js
@@ -4,31 +4,43 @@
 // arguments
 // return value
 
+// ES-5 
 
-class Car{
-    constructor(manufacturer, make, productionYear, isActive){
-        this.manufacturer= manufacturer, 
-        this.make = make,
-        this.productionYear = productionYear,
-        this.isActive = isActive
-    }
+// function sumarize(a, b){
+//     return a+ b;
+// }
 
-    isToyota(){
-        if(this.manufacturer === "toyota"){
-            return true
-        }
-        return false
-    }
-
-    returnName(){
-        return this.manufacturer + " " + this.make
-    }
-}
-
-let car = new Car("nissan", "Sentra", 1984, true)
-
-console.log(car)
-console.log(car.isToyota())
+// ES-6 
+let sumarize = (a, b) => a+b;
 
 
-console.log(car.returnName())
+console.log(sumarize(12,2))
+
+
+// class Car{
+//     constructor(manufacturer, make, productionYear, isActive){
+//         this.manufacturer= manufacturer, 
+//         this.make = make,
+//         this.productionYear = productionYear,
+//         this.isActive = isActive
+//     }
+
+//     isToyota(){
+//         if(this.manufacturer === "toyota"){
+//             return true
+//         }
+//         return false
+//     }
+
+//     returnName(){
+//         return this.manufacturer + " " + this.make
+//     }
+// }
+
+// let car = new Car("nissan", "Sentra", 1984, true)
+
+// console.log(car)
+// console.log(car.isToyota())
+
+
+// console.log(car.returnName())
