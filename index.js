9a4762d9ee2d10e79a723630798c54a462030860
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-06-28 10:57:53 +0800

    message

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-let customers=[ 
-    {customerName: "Joe", age: 43},
-    {customerName: "Alex", age: 22} 
-]
+let customers = ["Joe", {customerName: "Sue", age: 22}, 15, 15.60];
 
-// let newCustomer = {customerName: "Brianna", age: 24}
+// console.log(customers[1])
 
-let newCustomer = [{customerName: "Brianna", age: 24},
- {customerName: "Diane", age: 34}]
-
-// let newCustomersList = customers.concat(newCustomer)
-
-//ES-5 2009 version javascript
-let newCustomersList = customers.concat(newCustomer);
-
-console.log(newCustomersList)
-
-// Es- 6 2015 v js: 
-
-let newCustomersList2 = [...customers, ...newCustomer]
-
-console.log(newCustomersList2)
+for(customer in customers){
+    console.log(customers[customer])
+}
