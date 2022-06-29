c173321e9cdb7dc1103d2bd51491fb0faa2e4140
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-06-29 11:06:45 +0800

    added dynamic table example

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
-let counter = 0 
-const changeColor = () =>{
-    if(counter === 0 ){
-        document.getElementById("helloColor").style="color: red"
-        counter =1
-    }else if (counter ===1){
-        document.getElementById("helloColor").style="color: black"
-        counter =0
-    }
-    
+let data=[
+    {productName: "banana", price: 100, qty: 2, subtotal: 200},
+    {productName: "dragon fruit", price: 150, qty: 1, subtotal: 150},
+    {productName: "screwdriver", price: 10, qty: 1, subtotal: 10.10},
+]
+
+// https://www.javascripttutorial.net/javascript-array-sort/
+function sortTable(key){
+    // if sort ascended or descended
+    // this method works with number only
+    let sortedData = data.sort((x,y) =>{
+        return x[key] - y[key]
+    })
+    renderTable(sortedData)
 }
+
+function renderTable(data){
+    
+    let tableColumnsDefinition = Object.keys(data[0])
+    let tableHeader=""
+
+    this.data = data
+
+    tableColumnsDefinition.forEach((tableName)=>{
+        tableHeader+= `<th scope="col" onclick="sortTable('${tableName}')">${tableName}</th> \n` 
+    }) 
+
+    let tableBody= ""
+    data.forEach( row =>{
+        tableBody+= "<tr>\n";
+        // console.log(row)
+        tableColumnsDefinition.forEach((tableName)=>{
+            tableBody+= `<td>${row[tableName]}</td>\n`
+        })
+        tableBody+="</tr>\n"
+    })
+
+
+    let tableDefinition=`
+        <table class="table table-hover">
+        <thead>
+        <tr>
+           ${tableHeader}
+        </tr>
+        </thead>
+        <tbody>
+            ${tableBody}
+        </tbody>
+    </table>
+    `
+
+    document.getElementById("grid-table").innerHTML=tableDefinition
+    return tableDefinition
+
+}
+
+
+
+renderTable(data)
+
