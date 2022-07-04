69fd8ef576fdf232f7fee5e04d6c94745c74a51c
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-07-04 11:20:46 +0800

    added example for events

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// select id 
-document.getElementById("cell-1").setAttribute('class',"table-primary")
-// document.getElementById("main-table").setAttribute('class', "table table-primary" )
+// 
 
-
-// select class
-// change attribute
-// adding html into an object
+document.getElementById("cell-1").setAttribute('class','table-primary')
+// document
+// .getElementById("main-table")
+// .setAttribute('class', 
+// 'table table-primary')
 
 document.getElementById("cell-1").innerHTML=""
-document.getElementById("cell-1").innerHTML="new value"
-// getting value from doc object
+document.getElementById("cell-1").innerHTML="New Value"
 
 
-let value = document.getElementById("text-input").value
-console.log(value)
+let updateValue = () =>{
+    let value = document.getElementById("text-input").value
+    document.getElementById("cell-1").innerHTML=value
+}
+
